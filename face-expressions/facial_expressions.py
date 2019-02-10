b6f0a33ff5a7288351a7bbf3fd9eba4640bf3cc3
danhaus <danhaus95@gmail.com>
2019-02-10 00:20:33 +0000

    [ADD] getter for the facial expressions, the video capture is supposed to be laggy: see the desctiption below
    The video frame gets stuck whener the code is waiting for an emotion, which is the desired behaviour

--- face-expressions/facial_expressions.py
+++ face-expressions/facial_expressions.py
@@ -30,6 +30,15 @@ class myThread(threading.Thread):
 		self.name = name
 		self.q = mp.Queue()
 		self.stop = False
+		# dictionary to store players expressions under their id, None stands for no expressions
+		self.expressions = {0: None, 1: None} 
+
+	def get_expression(self, id_):
+		while True:
+			expression = self.expressions[id_]
+			if expression is not None:
+				return expression
+
 
 
 	def run(self):
@@ -42,16 +51,16 @@ class myThread(threading.Thread):
 		self.stop = True
 
 
-	def draw_face(self, rect, frame, shape):
+	def draw_face(self, id_, rect, frame, shape):
 		# Draw rectangle around the face
 		rect_left_top = (rect.left(), rect.top())
 		rect_right_bottom = (rect.right(), rect.bottom())
 		colour = (0,0,0)
-		if id == 0:
+		if id_ == 0:
 			colour = (255,0,0)
-		elif id == 1:
+		elif id_ == 1:
 			colour = (0,255,0)
-		elif id == 2:
+		elif id_ == 2:
 			colour = (0,0,255)
 		cv2.rectangle(frame, rect_left_top, rect_right_bottom, colour, 2)
 
@@ -61,7 +70,7 @@ class myThread(threading.Thread):
 			cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
 
-	def process_face(self, id, rect, shape):
+	def process_face(self, id_, rect, shape):
 		# Indexing is shifted by one!
 		# Get useful landmarks
 		top_lip = shape[62]
@@ -81,19 +90,26 @@ class myThread(threading.Thread):
 		# inner edges of eyebrows
 		eyebrow_inner_dist = np.linalg.norm(eyebrow_left_inner - eyebrow_right_inner)
 
-		print("Person:", id)
+		print("Person:", id_)
 
 		# Detect smiling
 		if (lips_hor_dist / rect_diag) > 0.3:
+			self.expressions[id_] = 'smiling'
 			print("Smiling")
 
 		# Detect opened mouth
-		if (lips_ver_dist / lips_hor_dist) > 0.2:
+		elif (lips_ver_dist / lips_hor_dist) > 0.2:
+			self.expressions[id_] = 'opened-mouth'
 			print("Opened mouth")
 
 		# Detect frowning
-		if (eyebrow_inner_dist / rect_diag) < 0.09:
+		elif (eyebrow_inner_dist / rect_diag) < 0.09:
+			self.expressions[id_] = 'frowning'
 			print("Frowning")
+		
+		# Otherwise None
+		else:
+			self.expressions[id_] = None
 
 		print()
 
@@ -124,14 +140,14 @@ class myThread(threading.Thread):
 			rects_lst.sort(key=lambda r: r.left())
 
 			# loop over the face detections
-			for id, rect in enumerate(rects_lst):
+			for id_, rect in enumerate(rects_lst):
 				shape = self.predictor(gray_frame, rect)
 				shape = face_utils.shape_to_np(shape)
-				self.draw_face(rect, frame, shape)
-				self.process_face(id, rect, shape)
+				self.draw_face(id_, rect, frame, shape)
+				self.process_face(id_, rect, shape)
 
 				if self.stop:
-					raise Exception("You've just wished to kill the thread. So did I.")
+					raise Exception("You've just wished to kill me. So did a suicide.")
 
 
 			# show the frame
@@ -150,6 +166,9 @@ if __name__ == '__main__':
 	thread1 = myThread(1, "Facial-Thread")
 	# Start new Threads
 	thread1.start()
+
+	counter = 0
+
 	while True:
 		frame = thread1.q.get()
 		cv2.imshow("Frame", frame)
@@ -159,5 +178,10 @@ if __name__ == '__main__':
 		if key == ord("q"):
 			thread1.stop_thread()
 			break
+
+		counter += 1
+		if counter % 1 == 0:
+			print(thread1.get_expression(0))
+
 
 	print ("Exiting Main Thread")
